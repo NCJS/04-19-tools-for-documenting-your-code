1b439e5b508e7502fc00b92de1b66861bd3c1168
andreas.groos <andreas.groos1@gmail.com>
2018-04-18 15:55:37 -0700

    added esdoc

---
+++ esdocs/script/search_index.js
@@ -0,0 +1,512 @@
+window.esdocSearchIndex = [
+  [
+    "jsdoc-talk-ncjs/sample code/a.js~dayingerman",
+    "function/index.html#static-function-dayInGerman",
+    "<span>dayInGerman</span> <span class=\"search-result-import-path\">jsdoc-talk-ncjs/sample code/a.js</span>",
+    "function"
+  ],
+  [
+    "jsdoc-talk-ncjs/sample code/a.js~joinarr",
+    "function/index.html#static-function-joinArr",
+    "<span>joinArr</span> <span class=\"search-result-import-path\">jsdoc-talk-ncjs/sample code/a.js</span>",
+    "function"
+  ],
+  [
+    "jsdoc-talk-ncjs/sample code/a.js~sum",
+    "function/index.html#static-function-sum",
+    "<span>sum</span> <span class=\"search-result-import-path\">jsdoc-talk-ncjs/sample code/a.js</span>",
+    "function"
+  ],
+  [
+    "jsdoc-talk-ncjs/sample code/b.js~times10",
+    "function/index.html#static-function-times10",
+    "<span>times10</span> <span class=\"search-result-import-path\">jsdoc-talk-ncjs/sample code/b.js</span>",
+    "function"
+  ],
+  [
+    "sample code/.external-ecmascript.js~array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
+    "sample code/.external-ecmascript.js~Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~arraybuffer",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer",
+    "sample code/.external-ecmascript.js~ArrayBuffer",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~boolean",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean",
+    "sample code/.external-ecmascript.js~Boolean",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~dataview",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView",
+    "sample code/.external-ecmascript.js~DataView",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~date",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date",
+    "sample code/.external-ecmascript.js~Date",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~error",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error",
+    "sample code/.external-ecmascript.js~Error",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~evalerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/EvalError",
+    "sample code/.external-ecmascript.js~EvalError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~float32array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Float32Array",
+    "sample code/.external-ecmascript.js~Float32Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~float64array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Float64Array",
+    "sample code/.external-ecmascript.js~Float64Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~function",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
+    "sample code/.external-ecmascript.js~Function",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~generator",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator",
+    "sample code/.external-ecmascript.js~Generator",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~generatorfunction",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/GeneratorFunction",
+    "sample code/.external-ecmascript.js~GeneratorFunction",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~infinity",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Infinity",
+    "sample code/.external-ecmascript.js~Infinity",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~int16array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int16Array",
+    "sample code/.external-ecmascript.js~Int16Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~int32array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int32Array",
+    "sample code/.external-ecmascript.js~Int32Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~int8array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int8Array",
+    "sample code/.external-ecmascript.js~Int8Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~internalerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/InternalError",
+    "sample code/.external-ecmascript.js~InternalError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~json",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON",
+    "sample code/.external-ecmascript.js~JSON",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~map",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map",
+    "sample code/.external-ecmascript.js~Map",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~nan",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN",
+    "sample code/.external-ecmascript.js~NaN",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~number",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number",
+    "sample code/.external-ecmascript.js~Number",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~object",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
+    "sample code/.external-ecmascript.js~Object",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~promise",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise",
+    "sample code/.external-ecmascript.js~Promise",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~proxy",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy",
+    "sample code/.external-ecmascript.js~Proxy",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~rangeerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RangeError",
+    "sample code/.external-ecmascript.js~RangeError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~referenceerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ReferenceError",
+    "sample code/.external-ecmascript.js~ReferenceError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~reflect",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect",
+    "sample code/.external-ecmascript.js~Reflect",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~regexp",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp",
+    "sample code/.external-ecmascript.js~RegExp",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~set",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set",
+    "sample code/.external-ecmascript.js~Set",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~string",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String",
+    "sample code/.external-ecmascript.js~String",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~symbol",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol",
+    "sample code/.external-ecmascript.js~Symbol",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~syntaxerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SyntaxError",
+    "sample code/.external-ecmascript.js~SyntaxError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~typeerror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError",
+    "sample code/.external-ecmascript.js~TypeError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~urierror",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/URIError",
+    "sample code/.external-ecmascript.js~URIError",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~uint16array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint16Array",
+    "sample code/.external-ecmascript.js~Uint16Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~uint32array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint32Array",
+    "sample code/.external-ecmascript.js~Uint32Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~uint8array",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array",
+    "sample code/.external-ecmascript.js~Uint8Array",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~uint8clampedarray",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8ClampedArray",
+    "sample code/.external-ecmascript.js~Uint8ClampedArray",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~weakmap",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap",
+    "sample code/.external-ecmascript.js~WeakMap",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~weakset",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakSet",
+    "sample code/.external-ecmascript.js~WeakSet",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~boolean",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean",
+    "sample code/.external-ecmascript.js~boolean",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~function",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
+    "sample code/.external-ecmascript.js~function",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~null",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/null",
+    "sample code/.external-ecmascript.js~null",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~number",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number",
+    "sample code/.external-ecmascript.js~number",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~object",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
+    "sample code/.external-ecmascript.js~object",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~string",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String",
+    "sample code/.external-ecmascript.js~string",
+    "external"
+  ],
+  [
+    "sample code/.external-ecmascript.js~undefined",
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/undefined",
+    "sample code/.external-ecmascript.js~undefined",
+    "external"
+  ],
+  [
+    "sample code/a.js",
+    "file/sample code/a.js.html",
+    "sample code/a.js",
+    "file"
+  ],
+  [
+    "sample code/b.js",
+    "file/sample code/b.js.html",
+    "sample code/b.js",
+    "file"
+  ],
+  [
+    "sample code/c.js",
+    "file/sample code/c.js.html",
+    "sample code/c.js",
+    "file"
+  ],
+  [
+    "sample code/c.js~numberlike:",
+    "typedef/index.html#static-typedef-Numberlike:",
+    "sample code/c.js~Numberlike:",
+    "typedef"
+  ],
+  [
+    "sample code/c.js~stationvisit",
+    "typedef/index.html#static-typedef-StationVisit",
+    "sample code/c.js~StationVisit",
+    "typedef"
+  ],
+  [
+    "sample code/docs/script/inherited-summary.js",
+    "file/sample code/docs/script/inherited-summary.js.html",
+    "sample code/docs/script/inherited-summary.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/inner-link.js",
+    "file/sample code/docs/script/inner-link.js.html",
+    "sample code/docs/script/inner-link.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/manual.js",
+    "file/sample code/docs/script/manual.js.html",
+    "sample code/docs/script/manual.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/patch-for-local.js",
+    "file/sample code/docs/script/patch-for-local.js.html",
+    "sample code/docs/script/patch-for-local.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/prettify/prettify.js",
+    "file/sample code/docs/script/prettify/prettify.js.html",
+    "sample code/docs/script/prettify/prettify.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/pretty-print.js",
+    "file/sample code/docs/script/pretty-print.js.html",
+    "sample code/docs/script/pretty-print.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/search.js",
+    "file/sample code/docs/script/search.js.html",
+    "sample code/docs/script/search.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/search_index.js",
+    "file/sample code/docs/script/search_index.js.html",
+    "sample code/docs/script/search_index.js",
+    "file"
+  ],
+  [
+    "sample code/docs/script/test-summary.js",
+    "file/sample code/docs/script/test-summary.js.html",
+    "sample code/docs/script/test-summary.js",
+    "file"
+  ],
+  [
+    "sample code/docs/scripts/linenumber.js",
+    "file/sample code/docs/scripts/linenumber.js.html",
+    "sample code/docs/scripts/linenumber.js",
+    "file"
+  ],
+  [
+    "sample code/docs/scripts/prettify/lang-css.js",
+    "file/sample code/docs/scripts/prettify/lang-css.js.html",
+    "sample code/docs/scripts/prettify/lang-css.js",
+    "file"
+  ],
+  [
+    "sample code/docs/scripts/prettify/prettify.js",
+    "file/sample code/docs/scripts/prettify/prettify.js.html",
+    "sample code/docs/scripts/prettify/prettify.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/inherited-summary.js",
+    "file/sample code/esdocs/script/inherited-summary.js.html",
+    "sample code/esdocs/script/inherited-summary.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/inner-link.js",
+    "file/sample code/esdocs/script/inner-link.js.html",
+    "sample code/esdocs/script/inner-link.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/manual.js",
+    "file/sample code/esdocs/script/manual.js.html",
+    "sample code/esdocs/script/manual.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/patch-for-local.js",
+    "file/sample code/esdocs/script/patch-for-local.js.html",
+    "sample code/esdocs/script/patch-for-local.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/prettify/prettify.js",
+    "file/sample code/esdocs/script/prettify/prettify.js.html",
+    "sample code/esdocs/script/prettify/prettify.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/pretty-print.js",
+    "file/sample code/esdocs/script/pretty-print.js.html",
+    "sample code/esdocs/script/pretty-print.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/search.js",
+    "file/sample code/esdocs/script/search.js.html",
+    "sample code/esdocs/script/search.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/search_index.js",
+    "file/sample code/esdocs/script/search_index.js.html",
+    "sample code/esdocs/script/search_index.js",
+    "file"
+  ],
+  [
+    "sample code/esdocs/script/test-summary.js",
+    "file/sample code/esdocs/script/test-summary.js.html",
+    "sample code/esdocs/script/test-summary.js",
+    "file"
+  ],
+  [
+    "sample code/index.js",
+    "file/sample code/index.js.html",
+    "sample code/index.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/api.js",
+    "file/sample code/yuidocs/api.js.html",
+    "sample code/yuidocs/api.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/assets/js/api-filter.js",
+    "file/sample code/yuidocs/assets/js/api-filter.js.html",
+    "sample code/yuidocs/assets/js/api-filter.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/assets/js/api-list.js",
+    "file/sample code/yuidocs/assets/js/api-list.js.html",
+    "sample code/yuidocs/assets/js/api-list.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/assets/js/api-search.js",
+    "file/sample code/yuidocs/assets/js/api-search.js.html",
+    "sample code/yuidocs/assets/js/api-search.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/assets/js/apidocs.js",
+    "file/sample code/yuidocs/assets/js/apidocs.js.html",
+    "sample code/yuidocs/assets/js/apidocs.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/assets/js/yui-prettify.js",
+    "file/sample code/yuidocs/assets/js/yui-prettify.js.html",
+    "sample code/yuidocs/assets/js/yui-prettify.js",
+    "file"
+  ],
+  [
+    "sample code/yuidocs/assets/vendor/prettify/prettify-min.js",
+    "file/sample code/yuidocs/assets/vendor/prettify/prettify-min.js.html",
+    "sample code/yuidocs/assets/vendor/prettify/prettify-min.js",
+    "file"
+  ]
+]
